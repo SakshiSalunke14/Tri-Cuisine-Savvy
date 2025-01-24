7106583f18d6d4f9c557a1d3f2b2a41677a98f93
SakshiSalunke14 <sakshisalunke1423@gmail.com>
2025-01-24 23:30:18 +0530

    messageProject initiating

---
+++ script.js
@@ -0,0 +1,66 @@
+function locomotiveAnimation() 
+{
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, 
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+  
+        ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    
+    ScrollTrigger.refresh();
+  }
+  locomotiveAnimation();
+  
+  function navbarAnimation() {
+    gsap.to("#nav-part1 svg", {
+      transform: "translateY(-100%)",
+      scrollTrigger: {
+        trigger: "#page1",
+        scroller: "#main",
+        start: "top 0",
+        end: "top -5%",
+        scrub: true,
+      },
+    });
+    gsap.to("#nav-part2 #links", {
+      transform: "translateY(-100%)",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#page1",
+        scroller: "#main",
+        start: "top 0",
+        end: "top -5%",
+        scrub: true,
+      },
+    });
+  }
+  navbarAnimation()
+  
